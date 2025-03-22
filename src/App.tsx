429885a3b80a60111ac3c29cf8b13f5566e8927e
Dominic Arrojado <dominicarrojado@gmail.com>
2025-03-22 08:33:03 +0800

    make app as host using module federation

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
-import { useState } from 'react'
+import { lazy, Suspense, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { ErrorBoundary } from "react-error-boundary"
+
+const Remote = lazy(
+	// @ts-expect-error @ts-ignore
+	async () => import('remote/remote-app'),
+);
 
 function App() {
   const [count, setCount] = useState(0)
@@ -16,7 +22,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Vite + React (I'm the host app)</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -28,6 +34,11 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+			<Suspense fallback="Loading...">
+        <ErrorBoundary fallback="Something went wrong...">
+          <Remote />
+        </ErrorBoundary>
+			</Suspense>
     </>
   )
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,35 @@
+
+import { federation } from '@module-federation/vite';
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { dependencies } from './package.json';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+	build: {
+		target: 'chrome89',
+	},
+  plugins: [
+    federation({
+			name: 'host',
+			remotes: {
+				remote: {
+					type: 'module',
+					name: 'remote',
+					entry: 'http://localhost:4174/remoteEntry.js',
+					entryGlobalName: 'remote',
+					shareScope: 'default',
+				},
+			},
+			exposes: {},
+			filename: 'remoteEntry.js',
+			shared: {
+				react: {
+					requiredVersion: dependencies.react,
+					singleton: true,
+				},
+			},
+		}),
+    react()
+  ],
 })
