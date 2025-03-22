7fcfb530036bd9a37d7884d8e525ddbb7bfc74bb
Dominic Arrojado <dominicarrojado@gmail.com>
2025-03-22 09:34:50 +0800

    implement deploy to github pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,34 +1,40 @@
 import { federation } from '@module-federation/vite';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import { dependencies } from './package.json';
 
 // https://vite.dev/config/
-export default defineConfig({
-  build: {
-    target: 'chrome89',
-  },
-  plugins: [
-    federation({
-      name: 'host',
-      remotes: {
-        remote: {
-          type: 'module',
-          name: 'remote',
-          entry: 'http://localhost:4174/remoteEntry.js',
-          entryGlobalName: 'remote',
-          shareScope: 'default',
+export default defineConfig(({ mode }) => {
+  const isProd = mode === 'production';
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    base: isProd ? '/host-app/' : '/',
+    build: {
+      target: 'chrome89',
+    },
+    plugins: [
+      federation({
+        name: 'host',
+        remotes: {
+          remote: {
+            type: 'module',
+            name: 'remote',
+            entry: `${env.VITE_REMOTE_APP_URL}/remoteEntry.js`,
+            entryGlobalName: 'remote',
+            shareScope: 'default',
+          },
         },
-      },
-      exposes: {},
-      filename: 'remoteEntry.js',
-      shared: {
-        react: {
-          requiredVersion: dependencies.react,
-          singleton: true,
+        exposes: {},
+        filename: 'remoteEntry.js',
+        shared: {
+          react: {
+            requiredVersion: dependencies.react,
+            singleton: true,
+          },
         },
-      },
-    }),
-    react(),
-  ],
+      }),
+      react(),
+    ],
+  };
 });
