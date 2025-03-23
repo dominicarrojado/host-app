85b1760f1abf5ab605d8c30c301853ec82459b9d
Dominic Arrojado <dominicarrojado@gmail.com>
2025-03-23 09:05:55 +0800

    add google fonts and implement PageHeaderLoading component

---
+++ src/components/PageHeaderLoading.tsx
@@ -0,0 +1,5 @@
+function PageHeaderLoading() {
+  return <div className="h-[50px] lg:h-[70px]" />;
+}
+
+export default PageHeaderLoading;
--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import { fileURLToPath } from 'url';
 import { federation } from '@module-federation/vite';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
@@ -38,5 +39,28 @@ export default defineConfig(({ mode }) => {
       react(),
       tailwindcss(),
     ],
+
+    resolve: {
+      alias: [
+        {
+          find: '@',
+          replacement: fileURLToPath(new URL('./src', import.meta.url)),
+        },
+        {
+          find: '@assets',
+          replacement: fileURLToPath(new URL('./src/assets', import.meta.url)),
+        },
+        {
+          find: '@components',
+          replacement: fileURLToPath(
+            new URL('./src/components', import.meta.url)
+          ),
+        },
+        {
+          find: '@lib',
+          replacement: fileURLToPath(new URL('./src/lib', import.meta.url)),
+        },
+      ],
+    },
   };
 });
